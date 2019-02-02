c96cdea4f7cf3676687941bc505d2f3f0610abda
Ivan Kuznetsov <kuzma.wm@gmail.com>
2019-02-03 00:11:08 +0200

    Fix column duplications caused by PR #3

---
+++ database/store_test.go
@@ -0,0 +1,77 @@
+package database
+
+import (
+	"github.com/dizzyfool/genna/model"
+	"go.uber.org/zap"
+	"testing"
+)
+
+func TestLive(t *testing.T) {
+
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.Encoding = "console"
+	logger, _ := config.Build()
+	url := `postgres://genna:genna@localhost:5432/genna?sslmode=disable`
+	db, _ := NewDatabase(url, logger)
+
+	store := NewStore(db)
+
+	createTestTable(store)
+
+	var schemas []string
+	schemas = append(schemas, model.PublicSchema)
+	rows, _ := store.queryTables(schemas)
+
+	t.Run("Should be 5 rows", func(t *testing.T) {
+		got := len(rows)
+		if got != 5 {
+			t.Errorf("There should be 5 rows but I see %v", got)
+		}
+	})
+
+	t.Run("Should not duplicate columns", func(t *testing.T) {
+		if got := isThereDuplicates(rows); got != false {
+			t.Error("Duplicated columns detected")
+		}
+	})
+
+}
+
+// Create table with complex constraints for testing
+func createTestTable(store *Store) {
+	store.db.Model().Exec(`
+	drop table if exists contracts;
+	drop table if exists employees;
+
+	CREATE TABLE employees (
+		id serial NOT null,
+		CONSTRAINT employees_pk PRIMARY KEY (id)
+	);
+
+	CREATE TABLE contracts (
+		employee_id int4 NOT NULL,
+		email varchar NOT NULL,
+		id1 int NOT NULL,
+		id2 varchar NOT NULL,
+		CONSTRAINT contracts_un1 CHECK (id1 > 0),
+		CONSTRAINT contracts_un2 UNIQUE (email),
+		CONSTRAINT contracts_un3 UNIQUE (email, employee_id),
+		CONSTRAINT contracts_un4 UNIQUE (id1, id2),
+		CONSTRAINT contracts_employees_fk FOREIGN KEY (employee_id) REFERENCES employees(id)
+	);
+	`)
+}
+
+// Check if there are duplicated columns
+func isThereDuplicates(rows []columnRow) bool {
+	encountered := map[string]bool{}
+	for _, v := range rows {
+		if encountered[v.TblName+v.ColumnName] == true {
+			return true
+		} else {
+			encountered[v.TblName+v.ColumnName] = true
+		}
+	}
+	return false
+}
