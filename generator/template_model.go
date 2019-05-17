00d6e4df07519d87f1f9d3874a38bffd5268634a
Dionid <don.diodos@gmail.com>
2019-05-17 12:59:34 +0300

    Changed template_model to generate struct

--- generator/template_model.go
+++ generator/template_model.go
@@ -7,32 +7,38 @@ import ({{range .Imports}}
     "{{.}}"{{end}}
 ){{end}}
 
-var Columns = struct { {{range .Models}}
-	{{.StructName}} struct{ 
+{{range .Models}}
+	type Columns{{.StructName}} struct{ 
 		{{range $i, $e := .Columns}}{{if $i}}, {{end}}{{.FieldName}}{{end}} string{{if .HasRelations}}
 
 		{{range $i, $e := .Relations}}{{if $i}}, {{end}}{{.FieldName}}{{end}} string{{end}}
-	}{{end}}
-}{ {{range .Models}}
-	{{.StructName}}: struct { 
-		{{range $i, $e := .Columns}}{{if $i}}, {{end}}{{.FieldName}}{{end}} string{{if .HasRelations}}
+	}
+{{end}}
 
-		{{range $i, $e := .Relations}}{{if $i}}, {{end}}{{.FieldName}}{{end}} string{{end}}
-	}{ {{range .Columns}}
+type ColumnsSt struct { {{range .Models}}
+	{{.StructName}} Columns{{.StructName}}{{end}}
+}
+
+var Columns = ColumnsSt{ {{range .Models}}
+	{{.StructName}}: Columns{{.StructName}}{ {{range .Columns}}
 		{{.FieldName}}: "{{.FieldDBName}}",{{end}}{{if .HasRelations}}
 		{{range .Relations}}
 		{{.FieldName}}: "{{.FieldName}}",{{end}}{{end}}
 	},{{end}}
 }
 
-var Tables = struct { {{range .Models}}
-	{{.StructName}} struct {
-		Name{{if .WithAlias}}, Alias{{end}} string
-	}{{end}}
-}{ {{range .Models}}
-	{{.StructName}}: struct {
-		Name{{if .WithAlias}}, Alias{{end}} string
-	}{ 
+{{range .Models}}
+type Table{{.StructName}} struct {
+	Name{{if .WithAlias}}, Alias{{end}} string
+}
+{{end}}
+
+type TablesSt struct { {{range .Models}}
+		{{.StructName}} Table{{.StructName}}{{end}}
+}
+
+var Tables = TablesSt { {{range .Models}}
+	{{.StructName}}: Table{{.StructName}}{ 
 		Name: "{{.TableName}}"{{if .WithAlias}},
 		Alias: "{{.TableAlias}}",{{end}}
 	},{{end}}
