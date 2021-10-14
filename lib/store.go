11345a202bc62b908110ebf08a3a4871e5575799
Antony Dovgal <anton.dovgal@semrush.com>
2021-10-14 23:13:14 +0300

    restore required field and add nolint

--- lib/store.go
+++ lib/store.go
@@ -49,6 +49,9 @@ func (r relation) Target() table {
 }
 
 type column struct {
+	// nolint this field is required by go-pg
+	tableName struct{} `pg:",discard_unknown_columns"`
+
 	Schema     string   `pg:"schema_name"`
 	Table      string   `pg:"table_name"`
 	Name       string   `pg:"column_name"`
